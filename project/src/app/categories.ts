c2d289d62889e93790253ef16186e904a09fbed4
Nelle <nurievaaa28@gmail.com>
2024-04-07 00:01:29 +0600

    init commit

---
+++ project/src/app/categories.ts
@@ -0,0 +1,23 @@
+export interface Category {
+    id: number;
+    name: string;
+}
+
+export const category = [
+    {
+        id: 1,
+        name: 'Готовые букеты'
+    },
+    {
+        id: 2,
+        name: 'Комнатные цветы'
+    },
+    {
+        id: 3,
+        name: 'Свадебные букеты'
+    },
+    {
+        id: 4,
+        name: 'ХЗ'
+    }
+]
