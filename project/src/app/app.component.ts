105a73bf504c9a72537c7c58799e93ae74749f18
Nelle <nurievaaa28@gmail.com>
2024-04-19 23:09:42 +0600

    init commit

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,10 +1,13 @@
 
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true, // Добавляем флаг standalone: true
+  standalone: true, 
+  imports: [RouterOutlet, RouterModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
