2643bf21110d5531060a83f494cc1537509eb230
Nelle <nurievaaa28@gmail.com>
2024-04-24 19:30:04 +0600

    Products & Login

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'main', pathMatch: 'full' },
     { path: 'main', component: HomeComponent, title: 'Главная страница' },
     { path: 'cart', component: CartComponent, title: 'Корзина' },
     {path: 'category/:categoryId',component: ProductDetailsComponent, title: 'Все продукты товара' },
+    {path: 'login',component: LoginComponent, title: 'Логин' },
 ];
