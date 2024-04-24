128cdfbb6bff86da1a70bae32637a37a1cc5f316
Nelle <nurievaaa28@gmail.com>
2024-04-24 20:42:25 +0600

    added cors

--- flowersshop/flowersshop/settings.py
+++ flowersshop/flowersshop/settings.py
@@ -51,8 +51,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
+]
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'flowersshop.urls'
 
 TEMPLATES = [
