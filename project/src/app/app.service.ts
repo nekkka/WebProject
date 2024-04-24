22d1bd5e0f7936627df161b58aaba4c6a0868f3c
Nelle <nurievaaa28@gmail.com>
2024-04-24 17:29:16 +0600

    Added new component and Appservice

--- project/src/app/app.service.ts
+++ project/src/app/app.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category, Product } from './models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  constructor(private client: HttpClient) { }
+
+  getCategories(): Observable<Category[]> {
+    return this.client.get<Category[]>(`http://127.0.0.1:8000/flower/categories/`);
+  }
+  getProductsByCategoryId(categoryId: number): Observable<Product[]> {
+    return this.client.get<Product[]>(`http://127.0.0.1:8000/flower/products/?category_id=${categoryId}`);
+  }
 }
---
+++ project/src/app/bouqet-details/bouqet-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bouqet-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './bouqet-details.component.html',
+  styleUrl: './bouqet-details.component.css'
+})
+export class BouqetDetailsComponent {
+
+}
